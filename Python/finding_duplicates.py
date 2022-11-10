6d335c3e152ff6010365aed5b101b6f5dfeba7c3
Aron Edward <aronf32@gmail.com>
2022-11-10 16:55:29 -0500

    removing duplicates

--- Python/finding_duplicates.py
+++ Python/finding_duplicates.py
@@ -10,8 +10,10 @@ data = json.load(f)
 for x in range(len(data)) :
     entry1 = data[x]
     networth1 = entry1["networth"]
+    height1 = entry1["height"]
     for y in range(x+1, len(data)) :
         entry2 = data[y]
         networth2 = entry2["networth"]
-        if (networth2==networth1 and similar(entry1["name"], entry2["name"])>0.5) :
+        height2 = entry2["height"]
+        if (networth2==networth1 and height2==height1 and similar(entry1["name"], entry2["name"])>0.5) :
             print(entry1["name"] + ", " + entry2["name"])
